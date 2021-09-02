d9705dd4a3c2de62b60634aa8e37440aad7cec55
Beatriz <biiadiiego@gmail.com>
2021-09-02 16:36:51 -0300

    [Correção de bugs na model Produto] - [02/09/2021]

--- demo/demo/src/main/java/org/ecommerce/demo/model/Produto.java
+++ demo/demo/src/main/java/org/ecommerce/demo/model/Produto.java
@@ -1,15 +1,14 @@
 package org.ecommerce.demo.model;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -39,7 +38,7 @@ public class Produto {
 	@NotBlank
 	private String marca;
 	
-	@NotBlank
+	@NotNull
 	private Double preco;
 	
 	@ManyToOne
