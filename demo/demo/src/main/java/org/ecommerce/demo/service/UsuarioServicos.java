5e865a274cbbc06d4765f1ff4f3559320f85a89d
diegobsailva10 <diegobdasilva12@gmail.com>
2021-09-10 11:25:00 -0300

    [Adicionando UsuarioServicos] - [10/09/2021]

--- demo/demo/src/main/java/org/ecommerce/demo/service/UsuarioServicos.java
+++ demo/demo/src/main/java/org/ecommerce/demo/service/UsuarioServicos.java
@@ -1,11 +1,14 @@
 package org.ecommerce.demo.service;
 
+import org.apache.commons.codec.binary.Base64;
 import org.ecommerce.demo.model.Usuario;
+import org.ecommerce.demo.model.UsuarioLogin;
 import org.ecommerce.demo.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.Charset;
 import java.util.Optional;
 
 @Service
@@ -14,14 +17,37 @@ public class UsuarioServicos {
     @Autowired
     UsuarioRepository repositorio;
 
-    public Optional <Object> cadastrarUsuario (Usuario novoUsuario){
+    public Optional <Object> cadastrarUsuario (Usuario novoUsuario) {
         return repositorio.findByEmail(novoUsuario.getEmail()).map(usuarioExistente -> {
-           return Optional.empty();
-        }).orElseGet((()->{
+            return Optional.empty();
+        }).orElseGet((() -> {
             BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
             String resultado = encoder.encode(novoUsuario.getSenha());
             novoUsuario.setSenha(resultado);
             return Optional.ofNullable(repositorio.save(novoUsuario));
         }));
+    }
+        public Optional<?> Login(UsuarioLogin usuarioParaAutenticar) {
+            return repositorio.findByEmail(usuarioParaAutenticar.getEmail()).map(usuarioExistente -> {
+                BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+                if (encoder.matches(usuarioParaAutenticar.getSenha(), usuarioExistente.getSenha())) {
+
+                    String estruturaBasic = usuarioParaAutenticar.getEmail() + ":" + usuarioParaAutenticar.getSenha();
+                    byte[] autorizacaoBase64 = Base64.encodeBase64(estruturaBasic.getBytes(Charset.forName("US-ASCII")));
+                    String autorizacaoHeader = "Basic " + new String(autorizacaoBase64);
+
+                    usuarioParaAutenticar.setToken(autorizacaoHeader);
+                    usuarioParaAutenticar.setNome(usuarioExistente.getNome());
+                    usuarioParaAutenticar.setSenha(usuarioExistente.getSenha());
+                    return Optional.ofNullable(usuarioParaAutenticar);
+
+                } else {
+                    return Optional.empty();
+                }
+
+            }).orElseGet(() -> {
+                return Optional.empty(); //
+        });
     }
 }
