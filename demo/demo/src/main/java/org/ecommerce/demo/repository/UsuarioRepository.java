b0b1e2df2267a4aca00967cb2f29d8902a136554
Bruno Luna <luna.brsilva@gmail.com>
2021-09-10 10:31:54 -0300

    [Inserção: Método FindByEmail no UsuarioRepository] - [10/09/2021]

--- demo/demo/src/main/java/org/ecommerce/demo/repository/UsuarioRepository.java
+++ demo/demo/src/main/java/org/ecommerce/demo/repository/UsuarioRepository.java
@@ -1,7 +1,7 @@
 package org.ecommerce.demo.repository;
 
 import java.util.List;
-
+import java.util.Optional;
 
 import org.ecommerce.demo.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,6 +9,29 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	
+	/**
+     * Método utilizado para pesquisar coluna nome ContainigIgnoreCase
+     * 
+     * @param nome do tipo String
+     * @return List de Usuarios
+     * @author G1
+     * @since 1.0
+     * 
+     */
+	
 	public List<Usuario> findAllByNomeContainingIgnoreCase(String nome);
+	
+	/**
+     * Método utilizado para pesquisar coluna email
+     * 
+     * @param email do tipo String
+     * @return Optional com Usuario
+     * @author G1
+     * @since 1.0
+     * 
+     */
+	
+	Optional<Usuario> findByEmail(String email);
 
 }
