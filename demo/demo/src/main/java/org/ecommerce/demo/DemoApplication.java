2abeddb1a4f34ed391203036ecbd86b03161feb2
Beatriz <biiadiiego@gmail.com>
2021-09-15 16:52:14 -0300

    [Adição: Redirecionamento Swagger] - [15/09/2021]

--- demo/demo/src/main/java/org/ecommerce/demo/DemoApplication.java
+++ demo/demo/src/main/java/org/ecommerce/demo/DemoApplication.java
@@ -2,9 +2,20 @@ package org.ecommerce.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class DemoApplication {
+	
+	@GetMapping
+	public ModelAndView swaggerUi() {
+		return new ModelAndView("redirect:/swagger-ui/");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
--- demo/demo/src/main/java/org/ecommerce/demo/controller/UsuarioController.java
+++ demo/demo/src/main/java/org/ecommerce/demo/controller/UsuarioController.java
@@ -10,7 +10,6 @@ import org.ecommerce.demo.model.UsuarioLogin;
 import org.ecommerce.demo.repository.UsuarioRepository;
 import org.ecommerce.demo.service.UsuarioServicos;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
