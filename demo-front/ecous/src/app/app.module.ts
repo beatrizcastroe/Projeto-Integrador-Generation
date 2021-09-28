cf4253f5f866488dd6fa93343f776a9b067e442d
Beatriz <biiadiiego@gmail.com>
2021-09-28 12:08:04 -0300

    [Correções] - [28/09/2021]

--- demo-front/ecous/src/app/app.module.ts
+++ demo-front/ecous/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
@@ -13,7 +12,6 @@ import { MenuComponent } from './menu/menu.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
     FooterComponent,
     LoginComponent,
     RegisterComponent,
