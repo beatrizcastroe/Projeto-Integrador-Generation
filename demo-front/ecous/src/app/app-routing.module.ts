cf5ab4459204f6dd61ae0f7f96ee34faa6863f35
unknown <barbara.cristianefs@gmail.com>
2021-09-29 11:50:12 -0300

    [Adição: rotas menu,footer e home] - [29/09/2021]

--- demo-front/ecous/src/app/app-routing.module.ts
+++ demo-front/ecous/src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
@@ -9,6 +12,9 @@ const routes: Routes = [
   
   {path:'login', component:LoginComponent},
   {path:'register', component:RegisterComponent},
+  {path:'menu', component:MenuComponent},
+  {path:'footer', component:FooterComponent},
+  {path:'home', component:HomeComponent},
 ];
 
 @NgModule({
