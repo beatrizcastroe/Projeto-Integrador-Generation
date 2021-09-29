09b59d3d9fbbd08f9256eacd698cd66b1dd72553
diegobsailva10 <diegobdasilva12@gmail.com>
2021-09-29 08:47:43 -0300

    Adicionando rotas

--- demo-front/ecous/src/app/app-routing.module.ts
+++ demo-front/ecous/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { RegisterComponent } from './register/register.component';
 const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch:'full'},
   
+  
   {path:'login', component:LoginComponent},
   {path:'register', component:RegisterComponent},
 ];
