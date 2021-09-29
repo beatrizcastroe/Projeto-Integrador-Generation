65d5921c84643e09933ae71c01586b51422ff563
diegobsailva10 <diegobdasilva12@gmail.com>
2021-09-29 08:39:43 -0300

    Adicionando Rotas

--- demo-front/ecous/src/app/app-routing.module.ts
+++ demo-front/ecous/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch:'full'},
+  
+  {path:'login', component:LoginComponent},
+  {path:'register', component:RegisterComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
