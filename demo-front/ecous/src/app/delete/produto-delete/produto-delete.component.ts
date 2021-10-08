5ee08d5c18a53c3cbb1dc9c3c1fc060f3d876516
diegobsailva10 <diegobdasilva12@gmail.com>
2021-10-08 15:47:15 -0300

    [Implementação DELETE Produto] - [08/10/2021]

--- demo-front/ecous/src/app/delete/produto-delete/produto-delete.component.ts
+++ demo-front/ecous/src/app/delete/produto-delete/produto-delete.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Produto } from 'src/app/model/Produto';
+import { CategoriaService } from 'src/app/service/categoria.service';
+import { ProdutoService } from 'src/app/service/produto.service';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-produto-delete',
@@ -7,9 +12,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProdutoDeleteComponent implements OnInit {
 
-  constructor() { }
+  produto: Produto = new Produto()
+  idProduto: number
 
-  ngOnInit(): void {
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private produtoService: ProdutoService,
+    private categoriaService: CategoriaService
+  ) { }
+
+  ngOnInit() {
+    window.scroll(0, 0)
+
+    if (environment.token == '') {
+      this.router.navigate(['/login'])
+    }
+
+    this.idProduto = this.route.snapshot.params['id']
+    this.findByIdProduto(this.idProduto)
+  }
+
+  findByIdProduto(idProduto: number) {
+    this.produtoService.getByIdProduto(idProduto).subscribe((resp: Produto) => {
+      this.produto = resp
+    })
+  }
+
+
+  apagar() {
+    this.produtoService.deleteProduto(this.idProduto).subscribe(() => {
+      alert('Produto apagado com sucesso')
+      this.router.navigate(['/backend'])
+    })
   }
 
 }
