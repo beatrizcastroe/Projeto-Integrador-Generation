f9c77b976248dea934e6341397908339cb569d8f
unknown <ana.carolinelr123@gmail.com>
2021-10-17 16:16:09 -0300

    [Alterações finais do footer e adição de links das redes sociais na página contato] - [17/10/2021]

--- demo-front/ecous/src/app/footer/footer.component.ts
+++ demo-front/ecous/src/app/footer/footer.component.ts
@@ -10,12 +10,18 @@ import { AlertasService } from '../service/alertas.service';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-  
+
   usuario: Usuario = new Usuario
   email: string
   emailNews: string
+  mensagem: string
 
-  validEmailNews:boolean = true;
+  validEmailNews: boolean = false
+  validCaracterEmail: boolean = false
+
+  feedback: boolean = false
+  validMensagem: boolean = false
+  validCaracter: boolean = false
 
   constructor(
     private router: Router,
@@ -23,40 +29,62 @@ export class FooterComponent implements OnInit {
 
   ) { }
 
-  
+
 
   ngOnInit() {
 
-  
+
   }
 
-  contato(){
-        this.router.navigate(['/contato'])
-      }
-  
-
-
-
-getNewsEmail(event: any) {
-  this.emailNews = event.target.value;
-  
-  if (this.emailNews.indexOf('@')== -1 || this.emailNews.indexOf('.') == -1 || this.emailNews.indexOf('com') == -1 ) {
-    this.validEmailNews = false;
-     
-  } else {
-    this.validEmailNews = true;
-    
-    
-  }
-}
-enviar(event:any){
-  if(this.validEmailNews==true ){
-    this.alertas.showAlertSuccess("Email cadastrado com sucesso")
-  }else{
-    this.alertas.showAlertDanger ("Email invalido")
+  contato() {
+    this.router.navigate(['/contato'])
   }
 
-  
+  getNewsEmail(event: any) {
+    this.email = event.target.value
+    if (this.email.length < 1) {
+      this.validEmailNews = true;
+      // a ValidEmail some após o primeiro caracter
+    } else {
+      this.validEmailNews = true;
+    }
+    if (this.email.indexOf('@' && '.') == -1) {
+      this.validCaracterEmail = true;
+    } else {
+      this.validCaracterEmail = false;
+    }
+  }
+  enviar(event: any) {
+    if (this.validEmailNews == true && this.validCaracterEmail == false) {
+      this.alertas.showAlertSuccess('E-mail cadastrado com sucesso!')
+    } else {
+      this.alertas.showAlertDanger('E-mail inválido, tente novamente')
+    }
+  }
 
-}
+  getMensagem(event: any) {
+    this.mensagem = event.target.value
+    if (this.mensagem.length < 1) {
+      this.validMensagem = true;
+      // a ValidMensagem some após o primeiro caracter
+    } else {
+      this.validMensagem = true;
+    }
+    if (this.mensagem.length > 1 && this.mensagem.length < 10) {
+      this.validCaracter = true;
+    } else {
+      this.validCaracter = false;
+    }
+  }
+
+
+
+  enviarModal(){
+    if(this.validMensagem == true && this.validEmailNews == true && this.validCaracterEmail == false){
+      this.alertas.showAlertSuccess("Agradeçemos o seu feedback!")
+    }else{
+      this.alertas.showAlertDanger ("Por favor, digite seu feedback!")
+    }
+  }
+  
 }
